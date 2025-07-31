f7f995f5db4a4073d71ddb1af3cec22f9113c73f
TheNightOwl-12 <aaa92054@gmail.com>
2025-07-31 15:27:35 +0800

    made the todo list items dynamically load in the page

--- frontend/index.js
+++ frontend/index.js
@@ -19,7 +19,33 @@ async function getTodos(){
 
     // handle the http response
     const todos = await response.json();
-    console.log(todos);
+    // console.log(todos);
+
+    //render html
+    const todoItemsContainer = document.getElementById("todo-items");
+    todos.forEach((todoItem) => {
+        console.log(todoItem.text);
+
+        //creat the list item elemnt
+        const todoListItem = document.createElement("li");
+
+
+        todoListItem.innerHTML = todoItem.text;
+
+        //create button
+        const updateBtn = document.createElement("button");
+        updateBtn.innerHTML = "Update";
+        const deleteBtn = document.createElement("button");
+        deleteBtn.innerHTML = "Delete";
+        todoListItem.appendChild(updateBtn);
+        todoListItem.appendChild(deleteBtn);
+
+        todoItemsContainer.appendChild(todoListItem);
+
+
+
+    });
+
 }
 
 getTodos();
